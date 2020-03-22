5a26aa85c61d63624a1cc87f6e9978dbf4b9ff98
Sebastian Källstedt <sebastian.kallstedt@codebet.se>
2020-03-22 16:00:30 +0100

    Updated backend with get and post functionallity for getting and saving todo lists, added jsonDB to simulate saving to actual database, updated package.json (#1)

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const JsonDB = require('node-json-db').JsonDB;
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+const bodyParser = require('body-parser');
 
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-const PORT = 3001
+const PORT = 3001;
+var db = new JsonDB(new Config('toDos', true, false, '/'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.get('/toDos', (req, res) => {
+  const toDos = db.getData('/');
+  console.log('toDos', toDos);
+
+  res.send(toDos);
+});
+
+app.post('/saveToDos', (req, res) => {
+  console.log('Hello from post!');
+  console.log(req);
+  console.log('Req.body: ' + req.body);
+  db.push('/', req.body);
+  res.send(req.body.data);
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
