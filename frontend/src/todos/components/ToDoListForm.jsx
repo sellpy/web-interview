d0d7b5b807dda44f587a2eb5fdc278ea1bfddd92
Sebastian <sebastian.kallstedt@codebet.se>
2020-03-22 15:32:24 +0100

    Updated toDoListForm to save onBlur to simulate autosave functionallity, uÃpdated dependencies, updated backend with jsonDB to simulate db, added get and post for getting and saving todos

--- frontend/src/todos/components/ToDoListForm.jsx
+++ frontend/src/todos/components/ToDoListForm.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
-import { makeStyles } from '@material-ui/styles'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Button from '@material-ui/core/Button'
-import DeleteIcon from '@material-ui/icons/Delete'
-import AddIcon from '@material-ui/icons/Add'
-import Typography from '@material-ui/core/Typography'
-import { TextField } from '../../shared/FormFields'
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import Typography from '@material-ui/core/Typography';
+import { TextField } from '../../shared/FormFields';
 
 const useStyles = makeStyles({
   card: {
@@ -28,39 +28,52 @@ const useStyles = makeStyles({
     flexDirection: 'column',
     flexGrow: 1
   }
-})
+});
 
 export const ToDoListForm = ({ toDoList, saveToDoList }) => {
-  const classes = useStyles()
-  const [todos, setTodos] = useState(toDoList.todos)
+  const classes = useStyles();
+  const [todos, setTodos] = useState(toDoList.todos);
 
   const handleSubmit = event => {
-    event.preventDefault()
-    saveToDoList(toDoList.id, { todos })
-  }
+    event.preventDefault();
+    saveToDoList(toDoList.id, { todos });
+  };
+
+  const handleBlur = event => {
+    event.preventDefault();
+    saveToDoList(toDoList.id, { todos });
+  };
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography component='h2'>
-          {toDoList.title}
-        </Typography>
+        <Typography component='h2'>{toDoList.title}</Typography>
         <form onSubmit={handleSubmit} className={classes.form}>
           {todos.map((name, index) => (
-            <div key={index} className={classes.todoLine}>
+            <div
+              key={index}
+              className={classes.todoLine}
+              tabIndex={index}
+              onBlur={handleBlur}
+            >
               <Typography className={classes.standardSpace} variant='h6'>
                 {index + 1}
               </Typography>
               <TextField
                 label='What to do?'
                 value={name}
+                key={index + 1}
+                tabIndex={index}
                 onChange={event => {
-                  setTodos([ // immutable update
+                  setTodos([
+                    // immutable update
                     ...todos.slice(0, index),
                     event.target.value,
                     ...todos.slice(index + 1)
-                  ])
+                  ]);
                 }}
+                onFocus={handleBlur}
+                onBlur={handleBlur}
                 className={classes.textField}
               />
               <Button
@@ -68,10 +81,11 @@ export const ToDoListForm = ({ toDoList, saveToDoList }) => {
                 color='secondary'
                 className={classes.standardSpace}
                 onClick={() => {
-                  setTodos([ // immutable delete
+                  setTodos([
+                    // immutable delete
                     ...todos.slice(0, index),
                     ...todos.slice(index + 1)
-                  ])
+                  ]);
                 }}
               >
                 <DeleteIcon />
@@ -83,7 +97,7 @@ export const ToDoListForm = ({ toDoList, saveToDoList }) => {
               type='button'
               color='primary'
               onClick={() => {
-                setTodos([...todos, ''])
+                setTodos([...todos, '']);
               }}
             >
               Add Todo <AddIcon />
@@ -95,5 +109,5 @@ export const ToDoListForm = ({ toDoList, saveToDoList }) => {
         </form>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
--- frontend/src/todos/components/ToDoLists.jsx
+++ frontend/src/todos/components/ToDoLists.jsx
@@ -1,73 +1,141 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ReceiptIcon from '@material-ui/icons/Receipt'
-import Typography from '@material-ui/core/Typography'
-import { ToDoListForm } from './ToDoListForm'
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ReceiptIcon from '@material-ui/icons/Receipt';
+import Typography from '@material-ui/core/Typography';
+import { ToDoListForm } from './ToDoListForm';
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const getPersonalTodos = () => {
-  return sleep(1000).then(() => Promise.resolve({
-    '0000000001': {
-      id: '0000000001',
-      title: 'First List',
-      todos: ['First todo of first list!']
-    },
-    '0000000002': {
-      id: '0000000002',
-      title: 'Second List',
-      todos: ['First todo of second list!']
+const getPersonalTodos = async () => {
+  // return sleep(1000).then(() =>
+  //   Promise.resolve({
+  //     '0000000001': {
+  //       id: '0000000001',
+  //       title: 'First List',
+  //       todos: ['First todo of first list!']
+  //     },
+  //     '0000000002': {
+  //       id: '0000000002',
+  //       title: 'Second List',
+  //       todos: ['First todo of second list!']
+  //     }
+  //   })
+  // );
+  try {
+    let result = await axios({
+      method: 'get',
+      url: 'http://localhost:3001/toDos',
+      // headers: {
+      //   Authorization:
+      //     'Basic ' +
+      //     btoa(
+      //       process.env.REACT_APP_USERNAME +
+      //         ':' +
+      //         process.env.REACT_APP_PASSWORD
+      //     ),
+      //   'Content-type': 'application/json'
+      // },
+      mode: 'cors'
+    });
+    console.log('saveData -> sampleRes', result);
+    return result.data;
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.data);
+      console.error(error.response.status);
+      console.error(error.response.headers);
+    } else if (error.request) {
+      console.error(error.request);
+    } else {
+      console.error('Error', error.message);
     }
-  }))
-}
+  }
+};
+
+const saveToDB = async toDoLists => {
+  console.log(toDoLists);
+  try {
+    let sampleRes = await axios({
+      method: 'post',
+      url: 'http://localhost:3001/saveToDos',
+      // headers: {
+      //   Authorization:
+      //     'Basic ' +
+      //     btoa(
+      //       process.env.REACT_APP_USERNAME +
+      //         ':' +
+      //         process.env.REACT_APP_PASSWORD
+      //     ),
+      //   'Content-type': 'application/json'
+      // },
+      mode: 'cors',
+      data: toDoLists
+    });
+    console.log('saveData -> sampleRes', sampleRes);
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.data);
+      console.error(error.response.status);
+      console.error(error.response.headers);
+    } else if (error.request) {
+      console.error(error.request);
+    } else {
+      console.error('Error', error.message);
+    }
+  }
+};
 
 export const ToDoLists = ({ style }) => {
-  const [toDoLists, setToDoLists] = useState({})
-  const [activeList, setActiveList] = useState()
+  const [toDoLists, setToDoLists] = useState({});
+  const [activeList, setActiveList] = useState();
 
   useEffect(() => {
-    getPersonalTodos()
-      .then(setToDoLists)
-  }, [])
+    getPersonalTodos().then(setToDoLists);
+  }, []);
 
-  if (!Object.keys(toDoLists).length) return null
-  return <Fragment>
-    <Card style={style}>
-      <CardContent>
-        <Typography
-          component='h2'
-        >
-          My ToDo Lists
-        </Typography>
-        <List>
-          {Object.keys(toDoLists).map((key) => <ListItem
-            key={key}
-            button
-            onClick={() => setActiveList(key)}
-          >
-            <ListItemIcon>
-              <ReceiptIcon />
-            </ListItemIcon>
-            <ListItemText primary={toDoLists[key].title} />
-          </ListItem>)}
-        </List>
-      </CardContent>
-    </Card>
-    {toDoLists[activeList] && <ToDoListForm
-      key={activeList} // use key to make React recreate component to reset internal state
-      toDoList={toDoLists[activeList]}
-      saveToDoList={(id, { todos }) => {
-        const listToUpdate = toDoLists[id]
-        setToDoLists({
-          ...toDoLists,
-          [id]: { ...listToUpdate, todos }
-        })
-      }}
-    />}
-  </Fragment>
-}
+  useEffect(() => {
+    console.log('ToDoLists were update with: ' + toDoLists);
+    if (toDoLists !== {}) saveToDB(toDoLists);
+  }, [toDoLists]);
+
+  if (!Object.keys(toDoLists).length) return null;
+  return (
+    <Fragment>
+      <Card style={style}>
+        <CardContent>
+          <Typography component='h2'>My ToDo Lists</Typography>
+          <List>
+            {Object.keys(toDoLists).map(key => (
+              <ListItem key={key} button onClick={() => setActiveList(key)}>
+                <ListItemIcon>
+                  <ReceiptIcon />
+                </ListItemIcon>
+                <ListItemText primary={toDoLists[key].title} />
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+      </Card>
+      {toDoLists[activeList] && (
+        <ToDoListForm
+          key={activeList} // use key to make React recreate component to reset internal state
+          toDoList={toDoLists[activeList]}
+          saveToDoList={(id, { todos }) => {
+            console.log('I am run now!');
+            const listToUpdate = toDoLists[id];
+            setToDoLists({
+              ...toDoLists,
+              [id]: { ...listToUpdate, todos }
+            });
+          }}
+        />
+      )}
+    </Fragment>
+  );
+};
