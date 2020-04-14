a81a1ee0f1259e77671b02182fac940c23d169f1
Johan Arnör <johan.arnor@gmail.com>
2020-04-14 18:28:23 +0200

    Simplified material-ui imports

--- frontend/src/todos/components/ToDoListForm.jsx
+++ frontend/src/todos/components/ToDoListForm.jsx
@@ -1,13 +1,8 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/styles'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Button from '@material-ui/core/Button'
+import { TextField, Card, CardContent, CardActions, Button, Typography} from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
 import AddIcon from '@material-ui/icons/Add'
-import Typography from '@material-ui/core/Typography'
-import { TextField } from '@material-ui/core'
 
 const useStyles = makeStyles({
   card: {
