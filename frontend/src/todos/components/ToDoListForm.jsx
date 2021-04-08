cfcd37fec9fa9c0728384d118240763849eaecc2
imadcollin <imad.collin@gmail.com>
2021-04-08 18:28:18 +0200

    Adding status and date for mock data

--- frontend/src/todos/components/ToDoListForm.jsx
+++ frontend/src/todos/components/ToDoListForm.jsx
@@ -1,72 +1,88 @@
-import React, { useState } from 'react'
-import { makeStyles } from '@material-ui/styles'
-import { TextField, Card, CardContent, CardActions, Button, Typography} from '@material-ui/core'
-import DeleteIcon from '@material-ui/icons/Delete'
-import AddIcon from '@material-ui/icons/Add'
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/styles";
+import {
+  TextField,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+  Checkbox,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
 
 const useStyles = makeStyles({
   card: {
-    margin: '1rem'
+    margin: "1rem",
   },
   todoLine: {
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center",
   },
   textField: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   standardSpace: {
-    margin: '8px'
+    margin: "8px",
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexGrow: 1
-  }
-})
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+  },
+});
 
 export const ToDoListForm = ({ toDoList, saveToDoList }) => {
-  const classes = useStyles()
-  const [todos, setTodos] = useState(toDoList.todos)
+  const classes = useStyles();
+  const [todos, setTodos] = useState(toDoList.todos);
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    saveToDoList(toDoList.id, { todos })
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    saveToDoList(toDoList.id, { todos });
+  };
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography component='h2'>
-          {toDoList.title}
-        </Typography>
+        <Typography component="h2">{toDoList.title}</Typography>
         <form onSubmit={handleSubmit} className={classes.form}>
           {todos.map((name, index) => (
             <div key={index} className={classes.todoLine}>
-              <Typography className={classes.standardSpace} variant='h6'>
+              <Typography className={classes.standardSpace} variant="h6">
                 {index + 1}
               </Typography>
               <TextField
-                label='What to do?'
+                label="What to do?"
                 value={name}
-                onChange={event => {
-                  setTodos([ // immutable update
+                onChange={(event) => {
+                  setTodos([
+                    // immutable update
                     ...todos.slice(0, index),
                     event.target.value,
-                    ...todos.slice(index + 1)
-                  ])
+                    ...todos.slice(index + 1),
+                  ]);
                 }}
                 className={classes.textField}
               />
+              <span style={{ padding: "5px", fontWeight: "bolder" }}>
+                Date:
+              </span>
+              {toDoList.date}
+              <span style={{ paddingLeft: "15px", fontWeight: "bolder" }}>
+                Compeleted:
+              </span>{" "}
+              <Checkbox checked={toDoList.completed}></Checkbox>
               <Button
-                size='small'
-                color='secondary'
+                size="small"
+                color="secondary"
                 className={classes.standardSpace}
                 onClick={() => {
-                  setTodos([ // immutable delete
+                  setTodos([
+                    // immutable delete
                     ...todos.slice(0, index),
-                    ...todos.slice(index + 1)
-                  ])
+                    ...todos.slice(index + 1),
+                  ]);
                 }}
               >
                 <DeleteIcon />
@@ -75,20 +91,20 @@ export const ToDoListForm = ({ toDoList, saveToDoList }) => {
           ))}
           <CardActions>
             <Button
-              type='button'
-              color='primary'
+              type="button"
+              color="primary"
               onClick={() => {
-                setTodos([...todos, ''])
+                setTodos([...todos, ""]);
               }}
             >
               Add Todo <AddIcon />
             </Button>
-            <Button type='submit' variant='contained' color='primary'>
+            <Button type="submit" variant="contained" color="primary">
               Save
             </Button>
           </CardActions>
         </form>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
