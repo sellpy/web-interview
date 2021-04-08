c0f0cef506ca4bd435566a89a6d37666e98d0148
imadcollin <imad.collin@gmail.com>
2021-04-08 18:26:44 +0200

    Restructure the api mock data

---
+++ frontend/src/MockApi.js
@@ -0,0 +1,25 @@
+const items = [
+    {
+        id: '0000000001',
+        title: 'First List',
+        todos:['First todo of first list!'],
+        completed: true,
+        date: '2020/01/01'
+    },
+    {
+        id: '0000000002',
+        title: 'Second List',
+        todos:['First todo of second list!'],
+        completed: false,
+        date: '2020/01/02'
+    },
+    {
+        id: '0000000003',
+        title: 'Third List',
+        todos:['First todo of third list!'],
+        completed: true,
+        date: '2020/01/03'
+    }
+];
+const getAllItems = () => items;
+export default { getAllItems };
