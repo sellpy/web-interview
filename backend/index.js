5a01cf9bdc19d59061cc1cbfb383723c3d35a7a6
imadcollin <imad.collin@gmail.com>
2021-04-10 14:35:18 +0200

    Add REST functions

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const mongoose = require("mongoose");
+const repository = require("./TodoRepository");
+var bodyParser = require("body-parser");
 
-app.use(cors());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/********************************************************** */
+const PORT = process.env.PORT || 8080;
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "http://localhost:8080",
 };
 
-const db = require("./models/todo.model");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+/********************************************************** */
+const db = require("./models/index");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -23,10 +32,52 @@ db.mongoose
     process.exit();
   });
 
-const PORT = process.env.PORT || 8080;
-
+/********************************************************** */
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to todo application." });
+  repository
+    .findAll()
+    .then((todos) => {
+      res.json(todos);
+    })
+    .catch((error) => console.log(error));
 });
 
+/********************************************************** */
+app.post("/", urlencodedParser, (req, res) => {
+  const { title, todos, completed, date } = req.body;
+  repository
+    .create(title, todos, completed, date)
+    .then((todo) => {
+      res.json(todo);
+    })
+    .catch((error) => console.log(error));
+});
+
+/********************************************************** */
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  repository
+    .deleteById(id)
+    .then((ok) => {
+      console.log(ok);
+      console.log(`Deleted record with id: ${id}`);
+      res.status(200).json([]);
+    })
+    .catch((error) => console.log(error));
+});
+/********************************************************** */
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const todo = {
+    title: req.body.title,
+    todos: req.body.todos,
+    completed: req.body.completed,
+  };
+  repository
+    .updateById(id, todo)
+    .then(res.status(200).json([]))
+    .catch((error) => console.log(error));
+});
+
+/********************************************************** */
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
