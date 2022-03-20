1178a717912142aa8e953a43eb5f78a1ad088bea
Moises Marquez <moy2010@gmail.com>
2022-03-20 12:48:01 +0100

    Add basic helmet headers

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 
 
 function createServer() {
   const app = express()
+  app.use(helmet())
   app.use(cors())
   app.use(express.json())
   app.use(require('./routes/todos'))
 
-// Global Error Handler
+  // Global Error Handler
   app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(err.status || 500).send({error: err.message});
