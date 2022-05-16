9caca2ee7c5f343b83222dfb1eb285f9d83fea60
Johan Arnör <johan@sellpy.se>
2022-05-16 15:13:41 +0200

    Formatted source file with prettier

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-  "env": {
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  "extends": "eslint:recommended",
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parserOptions": {
-    "ecmaVersion": 2018
+  parserOptions: {
+    ecmaVersion: 2018,
   },
-  "rules": {
-  }
-};
+  rules: {},
+}
--- frontend/src/todos/components/TodoLists.jsx
+++ frontend/src/todos/components/TodoLists.jsx
@@ -1,23 +1,33 @@
 import React, { Fragment, useState, useEffect } from 'react'
-import { Card, CardContent, List, ListItem, ListItemText, ListItemIcon, Typography } from '@mui/material'
+import {
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Typography,
+} from '@mui/material'
 import ReceiptIcon from '@mui/icons-material/Receipt'
 import { TodoListForm } from './TodoListForm'
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const getPersonalTodos = () => {
-  return sleep(1000).then(() => Promise.resolve({
-    '0000000001': {
-      id: '0000000001',
-      title: 'First List',
-      todos: ['First todo of first list!']
-    },
-    '0000000002': {
-      id: '0000000002',
-      title: 'Second List',
-      todos: ['First todo of second list!']
-    }
-  }))
+  return sleep(1000).then(() =>
+    Promise.resolve({
+      '0000000001': {
+        id: '0000000001',
+        title: 'First List',
+        todos: ['First todo of first list!'],
+      },
+      '0000000002': {
+        id: '0000000002',
+        title: 'Second List',
+        todos: ['First todo of second list!'],
+      },
+    })
+  )
 }
 
 export const TodoLists = ({ style }) => {
@@ -25,43 +35,40 @@ export const TodoLists = ({ style }) => {
   const [activeList, setActiveList] = useState()
 
   useEffect(() => {
-    getPersonalTodos()
-      .then(setTodoLists)
+    getPersonalTodos().then(setTodoLists)
   }, [])
 
   if (!Object.keys(todoLists).length) return null
-  return <Fragment>
-    <Card style={style}>
-      <CardContent>
-        <Typography
-          component='h2'
-        >
-          My Todo Lists
-        </Typography>
-        <List>
-          {Object.keys(todoLists).map((key) => <ListItem
-            key={key}
-            button
-            onClick={() => setActiveList(key)}
-          >
-            <ListItemIcon>
-              <ReceiptIcon />
-            </ListItemIcon>
-            <ListItemText primary={todoLists[key].title} />
-          </ListItem>)}
-        </List>
-      </CardContent>
-    </Card>
-    {todoLists[activeList] && <TodoListForm
-      key={activeList} // use key to make React recreate component to reset internal state
-      todoList={todoLists[activeList]}
-      saveTodoList={(id, { todos }) => {
-        const listToUpdate = todoLists[id]
-        setTodoLists({
-          ...todoLists,
-          [id]: { ...listToUpdate, todos }
-        })
-      }}
-    />}
-  </Fragment>
+  return (
+    <Fragment>
+      <Card style={style}>
+        <CardContent>
+          <Typography component='h2'>My Todo Lists</Typography>
+          <List>
+            {Object.keys(todoLists).map((key) => (
+              <ListItem key={key} button onClick={() => setActiveList(key)}>
+                <ListItemIcon>
+                  <ReceiptIcon />
+                </ListItemIcon>
+                <ListItemText primary={todoLists[key].title} />
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+      </Card>
+      {todoLists[activeList] && (
+        <TodoListForm
+          key={activeList} // use key to make React recreate component to reset internal state
+          todoList={todoLists[activeList]}
+          saveTodoList={(id, { todos }) => {
+            const listToUpdate = todoLists[id]
+            setTodoLists({
+              ...todoLists,
+              [id]: { ...listToUpdate, todos },
+            })
+          }}
+        />
+      )}
+    </Fragment>
+  )
 }
