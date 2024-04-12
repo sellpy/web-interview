238565a9e0ab0049dac9611e1786198cc21535fb
Johan Arnör <johan@sellpy.se>
2024-04-12 08:26:10 +0200

    Switched to modules and cleand up eslintrc

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,16 +1,11 @@
 module.exports = {
   env: {
-    commonjs: true,
-    es6: true,
     node: true,
   },
   extends: 'eslint:recommended',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {},
 }
