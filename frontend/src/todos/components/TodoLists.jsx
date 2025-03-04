9d3a29a05819a06d52d3073749548689d2b61700
Cia Broang <cia.broang@sj.se>
2025-03-04 17:08:18 +0100

    Remove simulate network stuff and add get data from fs backend.

--- frontend/src/todos/components/TodoLists.jsx
+++ frontend/src/todos/components/TodoLists.jsx
@@ -11,32 +11,15 @@ import {
 import ReceiptIcon from '@mui/icons-material/Receipt'
 import { TodoListForm } from './TodoListForm'
 
-// Simulate network
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
-const fetchTodoLists = () => {
-  return sleep(1000).then(() =>
-    Promise.resolve({
-      '0000000001': {
-        id: '0000000001',
-        title: 'First List',
-        todos: ['First todo of first list!'],
-      },
-      '0000000002': {
-        id: '0000000002',
-        title: 'Second List',
-        todos: ['First todo of second list!'],
-      },
-    })
-  )
-}
-
 export const TodoLists = ({ style }) => {
   const [todoLists, setTodoLists] = useState({})
   const [activeList, setActiveList] = useState()
 
   useEffect(() => {
-    fetchTodoLists().then(setTodoLists)
+    fetch('http://localhost:3001/data')
+      .then((res) => res.json())
+      .then(setTodoLists)
+      .catch((err) => console.error('Failed to fetch todo lists', err))
   }, [])
 
   if (!Object.keys(todoLists).length) return null
