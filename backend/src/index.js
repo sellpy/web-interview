87e6f8e0ccbc03ffa2366b2065b79e2126570eb3
Cia Broang <cia.broang@sj.se>
2025-03-04 16:57:12 +0100

    Continue set up Express server with file handling for reading and saving data to data.json with fs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import fs from 'fs'
 
 const app = express()
 
@@ -11,3 +12,30 @@ const PORT = 3001
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+
+const FILE_PATH = './data.json'
+
+const readData = () => {
+  try {
+    const data = fs.readFileSync(FILE_PATH, 'utf-8')
+    return JSON.parse(data)
+  } catch (error) {
+    console.error('Error when parsing data:', error)
+    throw error
+  }
+}
+
+app.get('/data', (req, res) => {
+  const storedData = readData()
+  res.json(storedData)
+})
+
+const saveData = (data) => {
+  try {
+    console.log('Saving data to data.json:', data)
+    fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, 2))
+  } catch (error) {
+    console.error('Error saving data to data.json:', error)
+  }
+}
+saveData()
